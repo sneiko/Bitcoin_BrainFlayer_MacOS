86a295377fd2bc9111322f145199ea295ab58ceb
Sergey Neikovich <s_neiko@outlook.com>
2020-05-10 22:52:08 +0300

    Initial Commit

---
+++ BitBrutter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BitBrutter
+//
+//  Created by Sergey Neikovich on 5/10/20.
+//  Copyright © 2020 F Developers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
